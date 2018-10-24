37126cd44174889fa12bc45574933fd26d509f2d
chaiwei <793262255@qq.com>
2018-10-25 01:02:29 +0800

    创建项目

---
+++ vant-demo-master/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+  	<div>
+  		<van-row>
+  			<router-view />
+  		</van-row>
+  		<van-row>
+	  		<van-tabbar v-model="active">
+		      <van-tabbar-item info="" to="/home">
+		        <span>首页</span>
+		        <img
+		          slot="icon"
+		          slot-scope="props"
+		          :src="props.active ? index.active : index.normal"
+		        >
+		      </van-tabbar-item>
+		      <van-tabbar-item info="3" to="/cart">
+		        <span>购物车</span>
+		        <img
+		          slot="icon"
+		          slot-scope="props"
+		          :src="props.active ? cart.active : cart.normal"
+		        >
+		      </van-tabbar-item>
+		      <van-tabbar-item info=""  to="/user">
+		        <span>个人中心</span>
+		        <img
+		          slot="icon"
+		          slot-scope="props"
+		          :src="props.active ? person.active : person.normal"
+		        >
+		      </van-tabbar-item>
+		    </van-tabbar>
+		   </van-row>
+  	</div>
+  </div>
+</template>
+
+<script>
+import {
+  Toast,Tabbar, TabbarItem
+} from 'vant';
+
+export default {
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem
+  },
+
+  data() {
+    return {
+      active: 0,
+      index: {
+        normal: 'src/assets/images/index/index-never.png',
+        active: 'src/assets/images/index/index.png'
+      },
+      cart: {
+        normal: './images/index/cart-never.png',
+        active: './images/index/cart.png'
+      },
+      person: {
+        normal: './images/index/person-never.png',
+        active: './images/index/person.png'
+      },
+      goods: {}
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  font-size: 16px;
+  background-color: #f8f8f8;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
---
+++ vant-demo-master/src/view/home/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="goods">
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页</div>
+    <div>我是首页1</div>
+    <div>我是首页2</div>
+    <div>我是首页3</div>
+    <div>我是首页4</div>
+    <div>我是首页5</div>
+    <div>我是首页6</div>
+    <div>我是首页7</div>
+    <div>我是首页8</div>
+    <div>我是首页9</div>
+    <div>我是首页10</div>
+    <div>我是首页11</div>
+    
+  </div>
+</template>
+
+<script>
+import {
+  Toast,Tabbar, TabbarItem
+} from 'vant';
+
+export default {
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem
+  },
+
+  data() {
+    return {
+      active: 0,
+      icon: {
+        normal: '//img.yzcdn.cn/icon-normal.png',
+        active: '//img.yzcdn.cn/icon-active.png'
+      },
+      goods: {}
+    };
+  },
+
+  methods: {
+    formatPrice() {
+      return '¥' + (this.goods.price / 100).toFixed(2);
+    },
+
+    onClickCart() {
+      this.$router.push('cart');
+    },
+
+    sorry() {
+      Toast('暂无后续逻辑~');
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.goods {
+  padding-bottom: 50px;
+
+  &-swipe {
+    img {
+      width: 7.5rem;
+      height: 7.5rem;
+      display: block;
+    }
+  }
+
+  &-title {
+    font-size: 16px;
+  }
+
+  &-price {
+    color: #f44;
+  }
+
+  &-express {
+    color: #999;
+    font-size: 12px;
+    padding: 5px 15px;
+  }
+
+  &-cell-group {
+    margin: 15px 0;
+
+    .van-cell__value {
+      color: #999;
+    }
+  }
+
+  &-tag {
+    margin-left: 5px;
+  }
+}
+</style>
